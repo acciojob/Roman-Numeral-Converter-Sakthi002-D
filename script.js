e7038742f3cd17209b421d0c5dd46107f3002458
Sakthi002-D <sakthivel.d002@gmail.com>
2026-04-19 13:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function convertToRoman(num) {
-  	const obj = {
+  	const map = {
       0:['M',1000], 
       1:['D', 500], 
       2:['C', 100], 
@@ -10,7 +10,7 @@ function convertToRoman(num) {
     };
   let result = "";
 
-  for (let [symbol, value] of obj) {
+  for (let [symbol, value] of map) {
     while (num >= value) {
       result += symbol;
       num -= value;
